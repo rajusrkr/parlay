774e02dc37e845e26346cf5048e3b223cc68bd72
Raju Sarkar <rajusarkaroo83@gmail.com>
2025-06-13 16:48:07 +0530

    add market table schema

--- apps/platform-api/src/db/schema.ts
+++ apps/platform-api/src/db/schema.ts
@@ -16,3 +16,19 @@ export const usersTable = pgTable("users", {
     accountCreatedOn: timestamp("account_created_on").defaultNow(),
     accountUpdatedOn: timestamp("account_updated_on)").$onUpdate(() => new Date())
 })
+
+export const CurrentMarketStatus = pgEnum("current_status", ["CLOSED", "OPEN", "SETTLED"])
+
+export const marketTable = pgTable("markets", {
+    id: serial("id").primaryKey(),
+    marketId: varchar("market_id", {length: 36}).notNull().unique(),
+    marketTitle: varchar("market_title", {length: 120}).notNull(),
+    side1: varchar("side_1", {length: 20}).notNull(),
+    side2: varchar("side_2", {length: 20}).notNull(),
+    marketStarts: timestamp("market_start").notNull(),
+    marketEnds: timestamp("market_ends").notNull(),
+    currentStatus: CurrentMarketStatus().default("CLOSED"),
+    winnerSide: varchar("winner_side", {length: 20}),
+    createdOn: timestamp("created_on").defaultNow(),
+    updatedOn: timestamp().$onUpdate(() => new Date())
+})
