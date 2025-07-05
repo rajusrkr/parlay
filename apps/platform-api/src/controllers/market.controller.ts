20dee85f4c401bf7bb129ae032eae34ca5e62fdc
Raju Sarkar <rajusarkaroo83@gmail.com>
2025-07-05 17:30:33 +0530

    sending proper data to client

--- apps/platform-api/src/controllers/market.controller.ts
+++ apps/platform-api/src/controllers/market.controller.ts
@@ -1,11 +1,13 @@
 import { Request } from "express";
 import { db } from "../db/dbConnection";
-import { marketTable, orderTable, priceData } from "../db/schema";
-import { asc, eq, sql } from "drizzle-orm";
+import { marketTable, priceData } from "../db/schema";
+import { asc, eq } from "drizzle-orm";
 
 const getAllMarket = async (req: Request, res: any) => {
   try {
-    const getAllMarkets = await db
+    
+    // get markets
+    const markets = await db
       .select({
         marketId: marketTable.marketId,
         marketTitle: marketTable.marketTitle,
@@ -13,27 +15,43 @@ const getAllMarket = async (req: Request, res: any) => {
         marketEnds: marketTable.marketEnds,
         currentStatus: marketTable.currentStatus,
         winnerSide: marketTable.winnerSide,
-        priceData: {
-          yesSide: priceData.yesSidePrice,
-          noSide: priceData.noSidePrice,
-          updatedOn: priceData.priceUpdatedOn
-        },
       })
-      .from(marketTable).leftJoin(priceData, eq(priceData.marketId, marketTable.marketId));
+      .from(marketTable).where(eq(marketTable.currentStatus, "OPEN"))
 
-    if (getAllMarket.length === 0) {
-      return res
-        .status(400)
-        .json({ success: false, message: "No market available" });
-    }
 
-    return res
-      .status(200)
-      .json({
-        success: true,
-        message: "Market fetched successfully",
-        markets: getAllMarkets,
-      });
+      if (markets.length === 0) {
+        return res.status(404).json({success: false, message: "No markets found"})
+      }
+
+      // get prices
+      const prices = await db.select().from(priceData).orderBy(asc(priceData.priceUpdatedOn))
+
+      if (prices.length === 0) {
+        return res.status(400).json({success: false, message: "Prices are not available"})
+      }
+
+
+      // group prices
+      const pricesMap = new Map<string, Array<{yes: {value: number, time: number}, no: {value: number, time: number}}>>()
+
+      for (const price of prices) {
+        // it creates an empty array with the id if it is not available
+        if (!pricesMap.has(price.marketId!)) {
+          pricesMap.set(price.marketId!, [])
+        }
+
+        // push prices to pricesMap by id
+        pricesMap.get(price.marketId!)!.push({yes: {time: price.priceUpdatedOn!, value: Number(price.yesSidePrice)}, no: {time: price.priceUpdatedOn!, value: Number(price.noSidePrice)}})
+      }
+
+      // merge those two, markets and pricemap
+      const merged = markets.map((market) => ({
+        ...market,
+        prices: pricesMap.get(market.marketId) || []
+      }))
+
+      return res.status(200).json({success: true, markets: merged})
+
   } catch (error) {
     console.log(error);
     return res
