f5c8868f7519f045eae6bd2e6ca78555c136ce16
Raju Sarkar <rajusarkaroo83@gmail.com>
2025-07-19 18:08:52 +0530

    modify the useWebsocket hook

--- apps/user-fe/src/hooks/useWebSocket.ts
+++ apps/user-fe/src/hooks/useWebSocket.ts
@@ -1,116 +1,50 @@
-import { useCallback, useEffect, useRef, useState } from "react";
+import { useRef, useState } from "react";
 
-// Web socket option interface
-interface UseWebSocketOptions {
-  onOpen?: (ws: WebSocket) => void;
-  onClose?: (event: CloseEvent) => void;
-  onMessage?: (event: MessageEvent) => void;
-  onError?: (event: Event) => void;
-  shouldReconnect: (event: CloseEvent) => boolean;
-  reconnectInterval?: number;
-  heartBeatInterval?: number;
-  heartBeatMessage?: string | (() => string); // its a string or a func that returns string, so we can send both simple string or a func => json.stringify({type: "data"})
-}
-
-export const useWebSocket = (url: string, options: UseWebSocketOptions) => {
-  const {
-    onOpen,
-    onClose,
-    onMessage,
-    onError,
-    shouldReconnect = () => true,
-    reconnectInterval = 3000,
-    heartBeatInterval = 15000,
-    heartBeatMessage = "ping",
-  } = options;
-
-  //   refs
-  const wsRef = useRef<WebSocket | null>(null);
-  const heartBeatRef = useRef<NodeJS.Timeout | null>(null);
-  const reconnectTimeout = useRef<NodeJS.Timeout | null>(null);
-  const messageQueue = useRef<string[]>([]);
+const useWebsocket = () => {
   const [isConnected, setIsConnected] = useState(false);
 
-  const connect = useCallback(() => {
-    // To ensure that the code only runs on the browser, not on the server side i.e nextjs server side safety
-    if (typeof window === "undefined") return;
+  const wsRef = useRef<WebSocket | null>(null);
 
-    // Opening a new connection
-    const ws = new WebSocket(url);
-    // Storing it in the wsRef
-    wsRef.current = ws;
+  // get socket identity token
+  const socketIdentity = document.cookie.split("=")[1];
 
-    // After successful ws connection
+  const wsSetup = () => {
+    const ws = new WebSocket("ws://localhost:8001");
+
     ws.onopen = () => {
+      if (isConnected || wsRef.current) {
+        console.log("Connection already exists");
+        return;
+      }
+      const message = {
+        eventType: "handShake",
+        data: { authToken: socketIdentity },
+      };
+      ws.send(JSON.stringify(message));
       setIsConnected(true);
-      // send data like auth token
-      if (onOpen) onOpen(ws);
-
-      // flush queue, if any message before the connection queue them and when the connection is ready fire that
-      while (messageQueue.current.length > 0) {
-        ws.send(messageQueue.current.shift()!);
-      }
-
-      // hearbeat
-      if (heartBeatInterval! > 0) {
-        heartBeatRef.current = setInterval(() => {
-          if (ws.readyState === WebSocket.OPEN) {
-            // If the heartBeatMessageType is func then pass a func else string
-            const msg =
-              typeof heartBeatMessage === "function"
-                ? heartBeatMessage()
-                : heartBeatMessage;
-
-            ws.send(msg!);
-          }
-        }, heartBeatInterval);
-      }
     };
 
-    ws.onmessage = onMessage || null;
-    ws.onerror = onError || null;
+    ws.onmessage = (msg) => {
+      const message = JSON.parse(msg.data);
+      console.log(message);
+    };
 
-    // Closing
-    ws.onclose = (event) => {
-      // Update state
+    ws.onclose = (ev) => {
       setIsConnected(false);
-      // clear interval for the heartBeatRef
-      if (heartBeatRef.current) clearInterval(heartBeatRef.current);
-      // if i provide any func
-      if (onClose) onClose(event);
-      // Should we reconnect, is it graceful close or something else
-      if (shouldReconnect(event)) {
-        reconnectTimeout.current = setTimeout(connect, reconnectInterval);
-      }
+      wsRef.current = null;
+      console.log("Connection has been closed, err:", ev.code, ev.reason);
     };
-  }, [url]);
 
-  // for sending message
-  const sendMessage = useCallback((message: string) => {
-    const ws = wsRef.current;
-
-    if (ws && ws.readyState === WebSocket.OPEN) {
-      ws.send(message);
-    } else {
-      messageQueue.current.push(message);
-    }
-  }, []);
-
-  // for graceful closing => user go away
-  const closeConnection = useCallback(() => {
-    reconnectTimeout.current && clearTimeout(reconnectTimeout.current);
-    heartBeatRef.current && clearInterval(heartBeatRef.current);
-    wsRef.current?.close();
-  }, []);
-
-  useEffect(() => {
-    // on comp mount connect
-    connect();
-    // on comp unmount close connection
-    return () => {
-      closeConnection();
+    ws.onerror = (err) => {
+      setIsConnected(false);
+      wsRef.current = null;
+      console.log("Error occured, err:", err);
     };
-  }, [connect]);
 
-  return { sendMessage, isConnected, close: closeConnection };
+    return ws
+  };
+
+  return { wsRef, isConnected, wsSetup };
 };
+
+export { useWebsocket };
