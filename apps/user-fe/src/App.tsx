beae100bddc7fa753b965bf9532ca5550c373834
Raju Sarkar <rajusarkaroo83@gmail.com>
2025-07-22 15:53:01 +0530

    rewrite routes in app.tsx

--- apps/user-fe/src/App.tsx
+++ apps/user-fe/src/App.tsx
@@ -1,44 +1,19 @@
-import { createBrowserRouter, RouterProvider } from "react-router";
+import { BrowserRouter, Route, Routes } from "react-router";
 import Home from "./pages/home";
-import OpenMarkets from "./pages/markets";
+import OpenMarkets from "./pages/markets"
 import Signin from "./pages/signin";
 import TradeCanvas from "./pages/TradeCanvas";
 
-const routes = createBrowserRouter([
-  {
-    path: "/",
-    element: (
-      <>
-      <Home />
-      </>
-    )
-  }, 
-  {
-    path: "/market",
-    element: (
-      <>
-      <OpenMarkets />
-      </>
-    )
-  },
-  {
-    path: "/market/:id",
-    element: (
-      <>
-        <TradeCanvas />
-      </>
-    )
-  },
-  {
-    path: "/auth/signin",
-    element: (
-      <>
-      <Signin />
-      </>
-    )
-  }
-])
-
 export default function App(){
-  return <RouterProvider router={routes}/>
+  return (
+    <BrowserRouter>
+      <Routes>
+        {/* General routes */}
+        <Route path="/" element = {<Home />}/>
+        <Route path="/market" element = {<OpenMarkets />}/>
+        <Route path="/auth/signin" element = {<Signin />}/>
+        <Route path="/market/:id" element = {<TradeCanvas />}/>
+      </Routes>
+    </BrowserRouter>
+  )
 }
