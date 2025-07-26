06e5a9f824173aeb23d4903d6a8619377f071d4c
Raju Sarkar <rajusarkaroo83@gmail.com>
2025-07-26 17:39:54 +0530

    add main layout

--- apps/admin-fe/src/App.tsx
+++ apps/admin-fe/src/App.tsx
@@ -1,11 +1,15 @@
 import { BrowserRouter, Route, Routes } from "react-router";
 import Home from "./pages/home";
+import MainLayout from "./layouts/main-layout";
 
 export default function App(){
   return(
     <BrowserRouter>
       <Routes>
+        <Route element = {<MainLayout />}>
+
         <Route path="/" element = {<Home />}/>
+        </Route>
       </Routes>
     </BrowserRouter>
   )
