b60c8add19f42956a6bddda8dbcf62c4beee90cc
Raju Sarkar <rajusarkaroo83@gmail.com>
2025-07-27 16:37:00 +0530

    schema changes according to the client side data

--- packages/shared/src/schemas/market.schema.ts
+++ packages/shared/src/schemas/market.schema.ts
@@ -1,9 +1,10 @@
 import { z } from "zod";
 
 export const MarketSchema = z.object({
-    marketTitle: z.string().max(50),
-    side1: z.string().max(15),
-    side2: z.string().max(15),
-    marketStarts: z.string().datetime({offset: true, message: "Invalid date format"}),
-    marketEnds: z.string().datetime({offset: true, message: "Invalid date format"}),
+    title: z.string().max(200),
+    overview: z.string().max(200),
+    settlement: z.string().max(200),
+    marketType: z.string().max(10),
+    marketStarts: z.number(),
+    marketEnds: z.number()
 })
