5dfe4be2f2ccb025ce1a9b9820d20726a8032cbc
Raju Sarkar <rajusarkaroo83@gmail.com>
2025-08-03 14:21:26 +0530

    updated market cards

--- apps/user-fe/src/pages/markets.tsx
+++ apps/user-fe/src/pages/markets.tsx
@@ -1,4 +1,4 @@
-import { Card, CardContent, CardTitle } from "@/components/ui/card";
+import OrderPlaceBtn from "@/components/order-place-btn";
 import { useWebsocket } from "@/hooks/useWebSocket";
 import { useMarketStore } from "@/stores/useMarketStore";
 import { useEffect } from "react";
@@ -6,7 +6,7 @@ import { Link } from "react-router";
 
 export default function Markets() {
   const { fetchMarkets, markets } = useMarketStore();
-  const {isConnected, connect, disconnect} = useWebsocket()
+  const { connect, disconnect } = useWebsocket();
 
   useEffect(() => {
     (async () => {
@@ -14,26 +14,34 @@ export default function Markets() {
     })();
 
     // Connect to ws server
-    connect()
+    connect();
 
     // Disconnect on umount
-    return () => {disconnect()}
+    return () => {
+      disconnect();
+    };
   }, [connect, disconnect]);
 
   return (
-    <div className="w-80 flex flex-col space-y-4 px-10">
-      {markets.map((market, i) => (
-        <Card key={i} className="p-2">
-          <Link to={`/market/${market.marketId}`}>
-            <CardTitle>{market.marketTitle}</CardTitle>
-          </Link>
-          <CardContent>
-            <p>Market Status: {market.currentStatus}</p>
-          </CardContent>
-        </Card>
-      ))}
-      <div>
-        {isConnected ? "Connected 🟢" : "Not connected🔴"}
+    <div className="max-w-7xl mx-auto p-4">
+      {/* For good responsiveness */}
+      <div className="grid gap-4 [grid-template-columns:repeat(auto-fit,minmax(250px,1fr))]">
+        {
+          markets.map((market, i) => (
+            <div key={i} className="border-2 border-black p-4 flex flex-col justify-between h-[260px]">
+              <Link to={`/market/${market.marketId}`}>
+                <span className="flex gap-2 mb-2">
+                  <img src={market.thumbnailImage} alt="Market Thumbnail" className="w-20 h-12 object-contain"/>
+                  <p className="font-semibold">{market.marketTitle}</p>
+                </span>
+              </Link>
+              <div className="flex gap-2.5 mt-auto pt-2">
+                <OrderPlaceBtn height="10" width="48" isBuy={true}/>
+                <OrderPlaceBtn height="10" width="48" isBuy={false}/>
+              </div>
+            </div>
+          ))
+        }
       </div>
     </div>
   );
