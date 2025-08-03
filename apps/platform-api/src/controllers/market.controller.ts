cc7d9aab59a0b630e29795707b2346ada4baba36
Raju Sarkar <rajusarkaroo83@gmail.com>
2025-08-03 13:29:00 +0530

    retutn thumbnail image, format with prettier

--- apps/platform-api/src/controllers/market.controller.ts
+++ apps/platform-api/src/controllers/market.controller.ts
@@ -1,59 +1,77 @@
 import { Request } from "express";
 
-
-import {db} from "db/src/dbConnection"
-import { marketTable, priceData} from "db/src/schema"
+import { db } from "db/src/dbConnection";
+import { marketTable, priceData } from "db/src/schema";
 import { asc, eq } from "drizzle-orm";
 
 const getAllMarket = async (req: Request, res: any) => {
   try {
-    
     // get markets
     const markets = await db
       .select({
         marketId: marketTable.marketId,
         marketTitle: marketTable.marketTitle,
+        thumbnailImage: marketTable.marketThumbnailImageUrl,
         marketStarts: marketTable.marketStarts,
         marketEnds: marketTable.marketEnds,
         currentStatus: marketTable.currentStatus,
         winnerSide: marketTable.winnerSide,
       })
-      .from(marketTable).where(eq(marketTable.currentStatus, "OPEN"))
+      .from(marketTable)
+      .where(eq(marketTable.currentStatus, "OPEN"));
 
+    if (markets.length === 0) {
+      return res
+        .status(404)
+        .json({ success: false, message: "No markets found" });
+    }
 
-      if (markets.length === 0) {
-        return res.status(404).json({success: false, message: "No markets found"})
+    // get prices
+    const prices = await db
+      .select()
+      .from(priceData)
+      .orderBy(asc(priceData.priceUpdatedOn));
+
+    if (prices.length === 0) {
+      return res
+        .status(400)
+        .json({ success: false, message: "Prices are not available" });
+    }
+
+    // group prices
+    const pricesMap = new Map<
+      string,
+      Array<{
+        yes: { value: number; time: number };
+        no: { value: number; time: number };
+      }>
+    >();
+
+    for (const price of prices) {
+      // it creates an empty array with the id if it is not available
+      if (!pricesMap.has(price.marketId!)) {
+        pricesMap.set(price.marketId!, []);
       }
 
-      // get prices
-      const prices = await db.select().from(priceData).orderBy(asc(priceData.priceUpdatedOn))
+      // push prices to pricesMap by id
+      pricesMap
+        .get(price.marketId!)!
+        .push({
+          yes: {
+            time: price.priceUpdatedOn!,
+            value: Number(price.yesSidePrice),
+          },
+          no: { time: price.priceUpdatedOn!, value: Number(price.noSidePrice) },
+        });
+    }
 
-      if (prices.length === 0) {
-        return res.status(400).json({success: false, message: "Prices are not available"})
-      }
+    // merge those two, markets and price map
+    const merged = markets.map((market) => ({
+      ...market,
+      prices: pricesMap.get(market.marketId) || [],
+    }));
 
-
-      // group prices
-      const pricesMap = new Map<string, Array<{yes: {value: number, time: number}, no: {value: number, time: number}}>>()
-
-      for (const price of prices) {
-        // it creates an empty array with the id if it is not available
-        if (!pricesMap.has(price.marketId!)) {
-          pricesMap.set(price.marketId!, [])
-        }
-
-        // push prices to pricesMap by id
-        pricesMap.get(price.marketId!)!.push({yes: {time: price.priceUpdatedOn!, value: Number(price.yesSidePrice)}, no: {time: price.priceUpdatedOn!, value: Number(price.noSidePrice)}})
-      }
-
-      // merge those two, markets and pricemap
-      const merged = markets.map((market) => ({
-        ...market,
-        prices: pricesMap.get(market.marketId) || []
-      }))
-
-      return res.status(200).json({success: true, markets: merged})
-
+    return res.status(200).json({ success: true, markets: merged });
   } catch (error) {
     console.log(error);
     return res
@@ -65,9 +83,13 @@ const getAllMarket = async (req: Request, res: any) => {
 const getPrices = async (req: Request, res: any) => {
   const data = req.body;
   console.log(data);
-  
+
   try {
-    const result = await db.select().from(priceData).orderBy(asc(priceData.priceUpdatedOn)).where(eq(priceData.marketId, data.marketId))
+    const result = await db
+      .select()
+      .from(priceData)
+      .orderBy(asc(priceData.priceUpdatedOn))
+      .where(eq(priceData.marketId, data.marketId));
 
     return res.status(200).json({ res: result });
   } catch (error) {
