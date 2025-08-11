3ed65fd02f4e165211bb655aa846abb9d8998610
Raju Sarkar <rajusarkaroo83@gmail.com>
2025-08-11 17:22:13 +0530

    add updated login and register page

--- apps/admin-fe/src/pages/login.tsx
+++ apps/admin-fe/src/pages/login.tsx
@@ -1,10 +1,5 @@
-import {
-  Button,
-  Card,
-  CardContent,
-  TextField,
-  Typography,
-} from "@mui/material";
+import { Button, Card, Input } from "@heroui/react";
+import { LogIn } from "lucide-react";
 import React, { useState } from "react";
 import { Link, useNavigate } from "react-router";
 
@@ -43,63 +38,76 @@ export default function Login() {
   };
 
   return (
-    <div className="flex flex-col justify-center items-center min-h-[90vh]">
-      <div className="mb-3">
-        <h2 className="text-4xl">
-          PredicX{" "}
-          <span className="border px-1 py-1 rounded bg-black text-white">
-            admin
-          </span>
-        </h2>
-      </div>
+    <div className="flex justify-center items-center min-h-[90vh]">
+      <Card className="w-96 h-96 flex justify-center items-center">
+        <div className="flex flex-col justify-center items-center">
+          <h2 className="text-3xl font-semibold">
+            PredictX{" "}
+            <span className="bg-black text-white px-2 rounded-xl pb-0.5">
+              admin
+            </span>
+          </h2>
+          <p className="flex text-lg items-center">
+            Login <LogIn />
+          </p>
+        </div>
 
-      <Card sx={{ width: 400 }}>
-        <CardContent>
-          <Typography gutterBottom variant="h3" component={"div"}>
-            Login
-          </Typography>
-
-          <form onSubmit={handleRegistration}>
-            <div className="flex flex-col space-y-3 pt-3">
-              <label htmlFor="emai;">Email</label>
-              <TextField
-                type="email"
-                label="Enter your email"
-                size="small"
-                required
-                onChange={(e) => setEmail(e.target.value)}
-              />
+        <form onSubmit={handleRegistration} className="space-y-3">
+          <div>
+            <div>
+              <label htmlFor="Market settlement">
+                <span className="text-sm font-semibold text-gray-500">
+                  Enter your email
+                </span>
+              </label>
             </div>
+            <Input
+              type="email"
+              placeholder="Enter email"
+              label="Email"
+              labelPlacement="inside"
+              variant="bordered"
+              required
+              className="w-80"
+              onChange={(e) => setEmail(e.target.value)}
+            />
+          </div>
 
-            <div className="flex flex-col space-y-3 pt-3">
-              <label htmlFor="password">Email</label>
-              <TextField
-                type="password"
-                label="Enter your password"
-                size="small"
-                required
-                onChange={(e) => setPassword(e.target.value)}
-              />
+          <div>
+            <div>
+              <label htmlFor="Market settlement">
+                <span className="text-sm font-semibold text-gray-500">
+                  Enter your password
+                </span>
+              </label>
             </div>
+            <Input
+              type="password"
+              placeholder="Enter password"
+              label="Password"
+              labelPlacement="inside"
+              variant="bordered"
+              required
+              className="w-80"
+              onChange={(e) => setPassword(e.target.value)}
+            />
+          </div>
 
-            <div className="flex justify-center pt-3">
-              <Button
-                type="submit"
-                variant="contained"
-                className="w-full"
-                style={{ backgroundColor: "black" }}
-              >
-                Login
-              </Button>
-            </div>
-          </form>
+          <div>
+            <Button className="w-80" color="primary" type="submit">
+              Login
+            </Button>
+          </div>
 
-          <div className="flex justify-center pt-3">
-            <Link to={"/auth/register"} className="text-blue-600 underline">
+          <div className="flex justify-center">
+            <Link
+              to={"/auth/register"}
+              className="text-blue-600 underline underline-offset-2 font-semibold"
+            >
               Register
             </Link>
           </div>
-        </CardContent>
+        </form>
       </Card>
     </div>
   );
--- apps/admin-fe/src/pages/register.tsx
+++ apps/admin-fe/src/pages/register.tsx
@@ -1,10 +1,5 @@
-import {
-  Button,
-  Card,
-  CardContent,
-  TextField,
-  Typography,
-} from "@mui/material";
+import { Button, Card, Input } from "@heroui/react";
+import { LogIn } from "lucide-react";
 import React, { useState } from "react";
 import { Link } from "react-router";
 
@@ -17,90 +12,116 @@ export default function Register() {
     e.preventDefault();
 
     try {
-      const sendReq = await fetch("http://localhost:8000/api/v0/admin/auth/register", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({email, name, password})
-      });
+      const sendReq = await fetch(
+        "http://localhost:8000/api/v0/admin/auth/register",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({ email, name, password }),
+        }
+      );
 
-      const res = await sendReq.json()
+      const res = await sendReq.json();
 
       console.log(res);
-      
     } catch (error) {
-        console.log(error);
+      console.log(error);
     }
   };
 
   return (
-    <div className="flex flex-col justify-center items-center min-h-[90vh]">
-      <div className="mb-3">
-        <h2 className="text-4xl">
-          PredicX{" "}
-          <span className="border px-1 py-1 rounded bg-black text-white">
-            admin
-          </span>
-        </h2>
-      </div>
+    <div className="flex justify-center items-center min-h-[90vh]">
+      <Card className="w-96 h-[480px] flex justify-center items-center">
+        <div className="flex flex-col justify-center items-center">
+          <h2 className="text-3xl font-semibold">
+            PredictX{" "}
+            <span className="bg-black text-white px-2 rounded-xl pb-0.5">
+              admin
+            </span>
+          </h2>
+          <p className="flex text-lg items-center">
+            Register <LogIn />
+          </p>
+        </div>
 
-      <Card sx={{ width: 400 }}>
-        <CardContent>
-          <Typography gutterBottom variant="h3" component={"div"}>
-            Register
-          </Typography>
-
-          <form onSubmit={handleRegistration}>
-            <div className="flex flex-col space-y-3">
-              <label htmlFor="name">Name</label>
-              <TextField
-                type="text"
-                label="Enter your name"
-                size="small"
-                required
-                onChange={(e) => setName(e.target.value)}
-              />
+        <form onSubmit={handleRegistration} className="space-y-3">
+          <div>
+            <div>
+              <label htmlFor="Market settlement">
+                <span className="text-sm font-semibold text-gray-500">
+                  Enter your name
+                </span>
+              </label>
             </div>
-            <div className="flex flex-col space-y-3 pt-3">
-              <label htmlFor="emai;">Email</label>
-              <TextField
-                type="email"
-                label="Enter your email"
-                size="small"
-                required
-                onChange={(e) => setEmail(e.target.value)}
-              />
-            </div>
-
-            <div className="flex flex-col space-y-3 pt-3">
-              <label htmlFor="password">Email</label>
-              <TextField
-                type="password"
-                label="Enter your password"
-                size="small"
-                required
-                onChange={(e) => setPassword(e.target.value)}
-              />
-            </div>
-
-            <div className="flex justify-center pt-3">
-              <Button
-                type="submit"
-                variant="contained"
-                className="w-full"
-                style={{ backgroundColor: "black" }}
-              >
-                Register
-              </Button>
-            </div>
-          </form>
-
-
-          <div className="flex justify-center pt-3">
-            <Link to={"/auth/login"} className="text-blue-600 underline">Login</Link>
+            <Input
+              type="text"
+              placeholder="Enter name"
+              label="Name"
+              labelPlacement="inside"
+              variant="bordered"
+              required
+              className="w-80"
+              onChange={(e) => setName(e.target.value)}
+            />
           </div>
-        </CardContent>
+
+          <div>
+            <div>
+              <label htmlFor="Market settlement">
+                <span className="text-sm font-semibold text-gray-500">
+                  Enter your email
+                </span>
+              </label>
+            </div>
+            <Input
+              type="email"
+              placeholder="Enter email"
+              label="Email"
+              labelPlacement="inside"
+              variant="bordered"
+              required
+              className="w-80"
+              onChange={(e) => setEmail(e.target.value)}
+            />
+          </div>
+
+          <div>
+            <div>
+              <label htmlFor="Market settlement">
+                <span className="text-sm font-semibold text-gray-500">
+                  Enter your password
+                </span>
+              </label>
+            </div>
+            <Input
+              type="password"
+              placeholder="Enter password"
+              label="Password"
+              labelPlacement="inside"
+              variant="bordered"
+              required
+              className="w-80"
+              onChange={(e) => setPassword(e.target.value)}
+            />
+          </div>
+
+          <div>
+            <Button className="w-80" color="primary" type="submit">
+              Register
+            </Button>
+          </div>
+
+          <div className="flex justify-center">
+            <Link
+              to={"/auth/login"}
+              className="text-blue-600 underline underline-offset-2 font-semibold"
+            >
+              Login
+            </Link>
+          </div>
+        </form>
       </Card>
     </div>
   );
