af2b14045bb528b34d6871d5d70a44bdda859cdf
Raju Sarkar <rajusarkaroo83@gmail.com>
2025-08-12 09:53:34 +0530

    declare types for outcomesAndPrices

--- packages/db/src/db-schema/market.ts
+++ packages/db/src/db-schema/market.ts
@@ -1,7 +1,13 @@
 import { bigint, jsonb, pgEnum, pgTable, serial, text, timestamp, varchar } from "drizzle-orm/pg-core";
 
-export const CurrentMarketStatus = pgEnum("current_status", ["NOT_STARTED","OPEN","SETTLED","CANCELLED"]);
-export const MarketCategory = pgEnum("market_category", ["SPORTS", "CRYPTO", "POLITICS"])
+export const CurrentMarketStatus = pgEnum("current_status", ["not_started","open","settled","cancelled"]);
+export const MarketCategory = pgEnum("market_category", ["sports", "crypto", "politics"])
+
+export interface OutcomeAndPrice {
+    outcome: string,
+    price: string,
+    qty: number
+}
 
 const market = pgTable("market", {
     // Market identity
@@ -16,7 +22,7 @@ const market = pgTable("market", {
     marketTitle: varchar("market_title", {length: 255}).notNull(),
     marketOverview: text("market_overview"),
     marketSettlement: text("market_settlement"),
-    currentStatus: CurrentMarketStatus().default("NOT_STARTED"),
+    currentStatus: CurrentMarketStatus().default("not_started"),
     marketCategory: MarketCategory(),
     marketThumbnailImageUrl: text("market_thumbnail_image_url"),
 
@@ -26,7 +32,7 @@ const market = pgTable("market", {
 
     
     // Outcome and prices, will store latest prices
-    outcomesAndPrices: jsonb("outcome_&_price").notNull(), 
+    outcomesAndPrices: jsonb("outcome_&_price").$type<OutcomeAndPrice[]>().notNull(), 
 
     // Winner
     winner: jsonb("winner"),
