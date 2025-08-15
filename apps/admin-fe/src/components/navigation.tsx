cadb1469be5fab24af09c023d7a70e5a976ef8f4
Raju Sarkar <rajusarkaroo83@gmail.com>
2025-08-15 19:59:05 +0530

    admin-fe: modify the navbar

--- apps/admin-fe/src/components/navigation.tsx
+++ apps/admin-fe/src/components/navigation.tsx
@@ -1,27 +1,32 @@
+import { Chip } from "@heroui/react";
 import { Link } from "react-router";
 
 export default function Navigation() {
   return (
-    <nav className="bg-gray-200 h-14 px-10 flex justify-between items-center">
+    <nav className="bg-gray-200 h-14 px-10 flex justify-between items-center shadow-md">
       <div className="flex gap-4">
-        <h2 className="text-3xl">
-          PredictX <span className="border px-1 py-1 rounded bg-black text-white">admin</span>
-        </h2>
-        <Link to={"/auth/login"} className="bg-blue-600 px-3 py-1 rounded text-white hover:bg-blue-700">Login</Link>
+        <Link to={"/"} className="text-3xl flex items-center">
+          PredictX
+          <span className="border px-2 pb-1 ml-1 rounded-xl bg-black text-white">
+            admin
+          </span>
+        </Link>
       </div>
 
-      <div className="flex gap-6 text-xl">
-        <div className="bg-gray-300 flex items-center px-4 rounded py-1 transition-all hover:shadow-2xl border-b-2 border-blue-600 hover:bg-gray-600 hover:text-white">
-          <Link to={"/"}>Open</Link>
+      <div className="flex items-center gap-2 text-xl">
+        <div>
+          <Link to={"/market/create"}>
+            <Chip size="lg" radius="md" variant="flat" color="primary">
+              Create Market
+            </Chip>
+          </Link>
         </div>
-        <div className="bg-gray-300 flex items-center px-4 rounded py-1 transition-all hover:shadow-2xl border-b-2 border-blue-600 hover:bg-gray-600 hover:text-white">
-            <Link to={"/"}>Settled</Link>
-        </div>
-        <div className="bg-gray-300 flex items-center px-4 rounded py-1 transition-all hover:shadow-2xl border-b-2 border-blue-600 hover:bg-gray-600 hover:text-white">
-            <Link to={"/"}>Will Open</Link>
-        </div>
-        <div className="bg-blue-600 text-white rounded px-2 py-1 hover:bg-blue-700 transition-all hover:shadow">
-            <Link to={"/create-market"}>Create Market</Link>
+        <div>
+          <Link to={"/auth/login"}>
+            <Chip size="lg" radius="md" color="primary">
+              Login
+            </Chip>
+          </Link>
         </div>
       </div>
     </nav>
