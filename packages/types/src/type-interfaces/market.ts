7ada38f36b3a6b532a1de61e5cf2045d063e928f
Raju Sarkar <rajusarkaroo83@gmail.com>
2025-08-20 18:03:25 +0530

    types: add OutcomeAndPrice and MarketData type interface

--- packages/types/src/type-interfaces/market.ts
+++ packages/types/src/type-interfaces/market.ts
@@ -1,6 +1,7 @@
 import { z } from "zod";
 
 const MarketCategoryEnum = z.enum(["sports", "politics", "crypto", "regular"])
+const MarketType = z.enum(["binary", "other"])
 
 const outcome = z.object({
     outcome: z.string(),
@@ -13,8 +14,36 @@ export const MarketSchema = z.object({
     overview: z.string().max(500),
     settlement: z.string().max(500),
     marketCategory: MarketCategoryEnum,
+    marketType: MarketType,
     marketStarts: z.number(),
     marketEnds: z.number(),
     outcomes: z.array(outcome),
     thumbnailImageUrl: z.string()
 })
+
+
+// Type interface
+interface OutcomeAndPrice {
+    price: string,
+    outcome: string,
+    tradedQty: number
+}
+
+
+interface MarketData {
+    marketId: string,
+    marketTitle: string,
+    marketOverview: string,
+    marketSettlement: string,
+    currentStatus: string,
+    marketCategory: string,
+    marketType: string,
+    thumbnailImage: string,
+    marketStarts: number,
+    marketEnds: number,
+    winnerSide: string,
+    outcomesAndPrices: OutcomeAndPrice[]
+}
+
+
+export type { MarketData }
