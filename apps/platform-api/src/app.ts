10b6006f1a2d773abd1939e882d1c055785eba85
Raju Sarkar <rajusarkaroo83@gmail.com>
2025-08-23 15:47:38 +0530

    init new vite app for user fe with next ui

--- apps/platform-api/src/app.ts
+++ apps/platform-api/src/app.ts
@@ -11,7 +11,7 @@ app.use(express.json())
 app.use(cookieParser())
 
 app.use(cors({
-    origin: ["http://localhost:5173", "http://localhost:5174"],
+    origin: ["http://localhost:5173", "http://localhost:5174", "http://localhost:5175"],
     methods: "GET, HEAD, PUT,PATCH, POST, DELETE",
     credentials: true
 }))
---
+++ apps/user-fe-2/src/App.tsx
@@ -0,0 +1,10 @@
+import { Button } from "@heroui/react";
+
+export default function App() {
+  return (
+    <div>
+      <h2 className="text-2xl text-blue-500">hey there</h2>
+      <Button>Hey</Button>
+    </div>
+  );
+}
