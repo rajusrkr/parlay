75936bcbc129c5242635a84e13245d3e05bb8a2f
Raju Sarkar <rajusarkaroo83@gmail.com>
2025-08-24 16:53:22 +0530

    user-fe-2: add market zustand store

---
+++ apps/user-fe-2/src/store/market.ts
@@ -0,0 +1,46 @@
+import { create } from "zustand";
+import { persist } from "zustand/middleware";
+import { type MarketData } from "types/src/index"
+import { PLATFORM_API_URI } from "../constants";
+
+
+interface Market {
+    isLoading: boolean;
+    isError: boolean;
+    errorMessage: string | null;
+
+    markets: MarketData[]
+
+    fetchMarket: () => Promise<void>
+}
+
+const usemarketStore = create(persist<Market>((set) => ({
+    isLoading: false,
+    isError: false,
+    errorMessage: null,
+
+    markets: [],
+
+    fetchMarket: async () => {
+        set({ isError: false, errorMessage: null, isLoading: true })
+        try {
+            const sendReq = await fetch(`${PLATFORM_API_URI}/market/get-markets`)
+
+            const res = await sendReq.json()
+
+            if (res.success) {
+                set({ isLoading: false, markets: res.markets })
+            } else {
+                set({ isLoading: false, isError: true, errorMessage: res.message })
+
+            }
+        } catch (error) {
+            console.log(error);
+            set({ isLoading: false, isError: true, errorMessage: "Uncaught error" })
+        }
+    }
+
+}), { name: "market-store" }))
+
+
+export { usemarketStore }
