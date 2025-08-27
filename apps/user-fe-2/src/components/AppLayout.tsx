588152680bfbcac506ed7651b10616ad7c9aed55
Raju Sarkar <rajusarkaroo83@gmail.com>
2025-08-28 02:41:20 +0530

    user-fe-2: add socket provider to app layout and market layout

--- apps/user-fe-2/src/components/AppLayout.tsx
+++ apps/user-fe-2/src/components/AppLayout.tsx
@@ -1,15 +1,18 @@
 import { Outlet } from "react-router";
 import Navbar from "./navbar";
 import MarketFilterbar from "./market-filter-bar";
+import { SocketProvider } from "./web-socket/socketContext";
 
-export default function AppHomeLayout(){
-    return (
-        <div>
-            <Navbar />
-            <MarketFilterbar />
-            <main>
-                <Outlet />
-            </main>
-        </div>
-    )
+export default function AppHomeLayout() {
+  return (
+    <div>
+      <Navbar />
+      <MarketFilterbar />
+      <main>
+        <SocketProvider>
+          <Outlet />
+        </SocketProvider>
+      </main>
+    </div>
+  );
 }
--- apps/user-fe-2/src/components/MarketLayout.tsx
+++ apps/user-fe-2/src/components/MarketLayout.tsx
@@ -1,13 +1,16 @@
 import { Outlet } from "react-router";
 import Navbar from "./navbar";
+import { SocketProvider } from "./web-socket/socketContext";
 
-export default function MarketLayout(){
-    return(
-        <div>
-            <Navbar />
-            <main>
-                <Outlet />
-            </main>
-        </div>
-    )
+export default function MarketLayout() {
+  return (
+    <div>
+      <Navbar />
+      <main>
+        <SocketProvider>
+          <Outlet />
+        </SocketProvider>
+      </main>
+    </div>
+  );
 }
