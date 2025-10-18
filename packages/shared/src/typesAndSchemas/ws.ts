6c58a1397a262fb7ac495154d5cfa6b04490c296
Raju Sarkar <rajusarkaroo83@gmail.com>
2025-10-18 14:27:22 +0530

    shared: modify ws types

--- packages/shared/src/typesAndSchemas/ws.ts
+++ packages/shared/src/typesAndSchemas/ws.ts
@@ -1,16 +1,48 @@
 import { type OutcomeInterface } from "../index"
 
-export type eventTypeEnum = "newBet" | "priceUpdateUI" | "lmsrCalculation" | "authAck"
+export type eventTypeEnum = "newBuyBet" | "newSellBet" | "priceUpdateUI" | "lmsrBuyCalculation" | "lmsrSellCalculation" | "authAck" | "handShake"
 
-interface newBetPayload {
-    eventType: eventTypeEnum
-    betId: string
-    data: {
-        outcomes: OutcomeInterface[]
-        betQty: number
-        betType: string,
-        selectedOutcomeIndex: number
-    }
+// Handshake interface
+interface handShakeData {
+    authToken: string
 }
 
-export { newBetPayload }
+// Auth ack interface
+interface authAckData {
+    message: string
+}
+
+// This will be used for both Buy side and Sell side order
+interface newBetData {
+    outcomes: OutcomeInterface[]
+    betQty: number
+    betType: string
+    selectedOutcomeIndex: number
+    betId: string
+}
+
+// Buy side order LMSR calculation ws response interface
+interface buyOrderlmsrCalculationInterfaceData {
+    outcome: OutcomeInterface[],
+    betQty: number,
+    betType: string,
+    betCost: number,
+    betId: string
+}
+
+// Sell side order LMSR calculation ws response interface
+interface sellOrderlmsrCalculationInterfaceData {
+    outcome: OutcomeInterface[],
+    betQty: number,
+    betType: string,
+    pnl: number,
+    betId: string
+}
+
+// All WS payload will use this
+interface wsData {
+    eventType: eventTypeEnum
+    data: {}
+}
+
+export { type wsData, type sellOrderlmsrCalculationInterfaceData, type buyOrderlmsrCalculationInterfaceData, type newBetData, type handShakeData, type authAckData }
